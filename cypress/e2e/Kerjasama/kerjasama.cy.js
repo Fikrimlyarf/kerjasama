acd255fab016b3e1ee75dbb95e823212ceaa6f0e
Fikrimlyarf <fikrimlyarf@gmail.com>
2025-02-26 16:10:00 +0700

    update

--- cypress/e2e/Kerjasama/kerjasama.cy.js
+++ cypress/e2e/Kerjasama/kerjasama.cy.js
@@ -1,8 +1,11 @@
 import Login from "../../pages/auth/Login";
 import Kerjasama from "../../pages/kerjasama/Kerjasama";
+import Keyword from "../../pages/keyword";
 
 const login = new Login();
 const kerma = new Kerjasama();
+const keyword = new Keyword();
+let dataKerma
 
 beforeEach(() => {
     login.visit();
@@ -10,4 +13,19 @@ beforeEach(() => {
     login.konfirmasiLogin();
     login.pilihModul("Admin Support Sevima", "Kerjasama");
     kerma.visitPage();
+
+    cy.fixture("kerma/kerjasama").then((data) => {
+        dataKerma = data;
+    });
 });
+
+describe("+ Positif Case", () => {
+    it('Admin menambahkan data kerjasama', () => {
+        dataKerma.listKerjasama.forEach((data) => {
+            kerma.aksiTambah();
+            kerma.inputDokumenKerjasama(data.noKerjasama);
+            kerma.inputDokumenMitra(data.noMitra)
+            keyword.autoSelected('name="id_jenis_dokumen"', data.jenisDokumen);
+        })
+    });
+})
--- cypress/pages/kerjasama/Kerjasama.js
+++ cypress/pages/kerjasama/Kerjasama.js
@@ -1,5 +1,19 @@
 class Kerjasama {
-    
+    visitPage() {
+        cy.contains("Kerjasama").should("be.visible").click();
+    }
+
+    aksiTambah() {
+        cy.contains("Tambah Data").should("be.visible").click();
+    }
+
+    inputDokumenKerjasama(dokumen) {
+        cy.get('#form-control-nomor_dokumen').clear().type(dokumen);
+    }
+
+    inputDokumenMitra(dokumen) {
+        cy.get('#form-control-nomor_dokumen_mitra').clear().type(dokumen);
+    }
 }
 
 export default Kerjasama;
