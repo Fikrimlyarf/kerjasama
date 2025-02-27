f4818ede20058a86e09df82147ee2d5f8058dca4
Fikrimlyarf <fikrimlyarf@gmail.com>
2025-02-27 19:54:34 +0700

    add kerjasama

--- cypress/e2e/Kerjasama/kerjasama.cy.js
+++ cypress/e2e/Kerjasama/kerjasama.cy.js
@@ -5,7 +5,8 @@ import Keyword from "../../pages/keyword";
 const login = new Login();
 const kerma = new Kerjasama();
 const keyword = new Keyword();
-let dataKerma
+let dataKerma;
+const dokumen = "cypress/fixtures/file-upload/lorem-ipsum.pdf";
 
 beforeEach(() => {
     login.visit();
@@ -20,12 +21,49 @@ beforeEach(() => {
 });
 
 describe("+ Positif Case", () => {
-    it('Admin menambahkan data kerjasama', () => {
+    it("Admin menambahkan data kerjasama", () => {
         dataKerma.listKerjasama.forEach((data) => {
             kerma.aksiTambah();
+            keyword.autoSelected('name="id_unit_kerja"', data.unit);
+            kerma.inputJudulKerjasama(data.judul);
+            keyword.autoSelected('name="id_mitra"', data.mitra);
             kerma.inputDokumenKerjasama(data.noKerjasama);
-            kerma.inputDokumenMitra(data.noMitra)
+            kerma.inputDokumenMitra(data.noMitra);
             keyword.autoSelected('name="id_jenis_dokumen"', data.jenisDokumen);
-        })
+            kerma.inputDeskripsiKerjasama(data.deskripsi);
+            keyword.autoSelected('name="id_sumber_dana"', data.dana);
+            kerma.inputAnggran(data.anggaran);
+            kerma.inputTglAwal(data.tglAwal);
+            kerma.inputTglAkhir(data.tglAkhir);
+            keyword.autoSelected('name="id_status_kerjasama"', data.status);
+            kerma.dokumen(dokumen, { fileName: "dokumen-kerjasama.pdf" });
+
+            kerma.pihak1().within(() => {
+                kerma.pihakDari(data.pihak1);
+                kerma.alamatPihak(data.alamat, 1, "alamat");
+
+            });
+            dataKerma.penanggungJawab.pj1.forEach((pj, index) => {
+                Object.keys(pj).forEach((key) => {
+                    kerma.penanggungJawab1(pj[key], index, key);
+                });
+                if (index < dataKerma.penanggungJawab.pj1.length - 1) {
+                    kerma.aksiTambahPenanggungJawab1();
+                }
+            });
+
+            kerma.alamatPihak(data.alamat, 2, "alamat");
+
+            dataKerma.penanggungJawab.pj2.forEach((pj, index) => {
+                Object.keys(pj).forEach((key) => {
+                    kerma.penanggungJawab2(pj[key], index, key);
+                });
+                if (index < dataKerma.penanggungJawab.pj2.length - 1) {
+                    kerma.aksiTambahPenanggungJawab2();
+                }
+            });
+
+            kerma.aksiSimpan();
+        });
     });
-})
+});
--- cypress/pages/kerjasama/Kerjasama.js
+++ cypress/pages/kerjasama/Kerjasama.js
@@ -7,12 +7,82 @@ class Kerjasama {
         cy.contains("Tambah Data").should("be.visible").click();
     }
 
+    aksiSimpan() {
+        cy.contains("Simpan").should("be.visible").click();
+    }
+
     inputDokumenKerjasama(dokumen) {
-        cy.get('#form-control-nomor_dokumen').clear().type(dokumen);
+        cy.get("#form-control-nomor_dokumen").clear().type(dokumen);
     }
 
     inputDokumenMitra(dokumen) {
-        cy.get('#form-control-nomor_dokumen_mitra').clear().type(dokumen);
+        cy.get("#form-control-nomor_dokumen_mitra").clear().type(dokumen);
+    }
+
+    inputJudulKerjasama(judul) {
+        cy.get("#form-control-judul_kerjasama").clear().type(judul);
+    }
+
+    inputDeskripsiKerjasama(deskripsi) {
+        cy.get("#form-control-deskripsi").clear().type(deskripsi);
+    }
+
+    inputAnggran(anggaran) {
+        cy.get("#form-control-anggaran").clear().type(anggaran);
+    }
+
+    inputTglAwal(tglAwal) {
+        cy.get("#form-control-tanggal_mulai_berlaku").type(tglAwal);
+    }
+
+    inputTglAkhir(tglAkhir) {
+        cy.get("#form-control-tanggal_akhir_berlaku").type(tglAkhir);
+    }
+
+    dokumen(dok) {
+        cy.get("#form-control-id_dokumen").selectFile(dok);
+    }
+
+    pihak1() {
+        return cy.get("#pihak-ke1");
+    }
+
+    pihakDari(pihak) {
+        cy.get('[data-testid="radio_penanggungJawab.1.pihak_penanggung_jawab.jenis_pihak"]')
+            .contains(pihak)
+            .click();
+    }
+
+    aksiTambahPenanggungJawab1() {
+        cy.contains("Tambah Penanggung Jawab Pihak ke 1").should("be.visible").click();    
+    }
+
+    aksiTambahPenanggungJawab2() {
+        cy.contains("Tambah Penanggung Jawab Pihak ke 2").should("be.visible").click();    
+    }
+
+    alamatPihak(alamat, index, elemet ) {
+        
+        cy.wait(1000);
+        cy.get(`[name="penanggungJawab.${index}.pihak_penanggung_jawab.${elemet}"]`)
+            .should("be.visible")
+            .type(alamat);
+    }
+
+    penanggungJawab1(value, index, elemet) {
+        cy.wait(1000);        
+        cy.get(`[name="penanggungJawab.1.penanggung_jawab.${index}.${elemet}"]`)
+            .should("be.visible")
+            .clear()
+            .type(value);
+    }
+
+    penanggungJawab2(value, index, elemet) {
+        cy.wait(1000);        
+        cy.get(`[name="penanggungJawab.2.penanggung_jawab.${index}.${elemet}"]`)
+            .should("be.visible")
+            .clear()
+            .type(value);
     }
 }
 
